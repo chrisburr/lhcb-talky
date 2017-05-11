37a5a68324f278e610f4cd10714465cc0379ca22
Christopher Burr <christopher.burr@cern.ch>
2017-05-11 19:45:46 +0200

    Fix updating of upload key

--- talky/database_events.py
+++ talky/database_events.py
@@ -22,33 +22,49 @@ def delete_file(mapper, connection, target):
             pass
 
 
-@listens_for(db.session, 'after_flush')
-def monitor_db(session, flush_context):
+@listens_for(db.session, 'before_flush')
+def monitor_db_before_flush(session, flush_context, instances):
     """Monitor for changes in the database"""
     changed_objects = session.new.union(session.dirty)
     for obj in changed_objects:
         if isinstance(obj, Talk):
-            talk_changed(session, obj)
-        if isinstance(obj, Submission):
-            submission_received(session, obj)
-        if isinstance(obj, Comment):
-            new_comment(session, obj)
+            update_upload_key(session, obj)
 
 
-def talk_changed(session, talk):
-    """If the speaker changes generate a new modified key and notify them"""
+def update_upload_key(talk):
+    """If the speaker changes generate a new modified key"""
     attribute_state = inspect(talk).attrs.get('speaker')
     # Check if the speaker has been updated
     history = attribute_state.history
     if history.has_changes():
         # Update the upload key
         talk.upload_key = secrets.token_urlsafe()
-        session.commit()
+
+
+@listens_for(db.session, 'after_flush')
+def monitor_db_after_flush(session, flush_context):
+    """Monitor for changes in the database"""
+    changed_objects = session.new.union(session.dirty)
+    for obj in changed_objects:
+        if isinstance(obj, Talk):
+            talk_changed(obj)
+        if isinstance(obj, Submission):
+            submission_received(obj)
+        if isinstance(obj, Comment):
+            new_comment(obj)
+
+
+def talk_changed(talk):
+    """If the speaker changes notify them"""
+    attribute_state = inspect(talk).attrs.get('speaker')
+    # Check if the speaker has been updated
+    history = attribute_state.history
+    if history.has_changes():
         # Send a new email to the speaker
         messages.send_talk_assgined(talk)
 
 
-def submission_received(session, submission):
+def submission_received(submission):
     """Send notifications if this is the first submission"""
     attribute_state = inspect(submission).attrs.get('version')
     # Check if the speaker has been updated
@@ -58,7 +74,7 @@ def submission_received(session, submission):
         messages.send_new_talk_available(submission)
 
 
-def new_comment(session, comment):
+def new_comment(comment):
     """Send notifications of new comments"""
     attribute_state = inspect(comment).attrs.get('comment')
     # Check if the speaker has been updated
